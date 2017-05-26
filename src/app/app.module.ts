2b323825b7bebf20da8823db3edc0ede37b4b8db
Kurt King <kurtaking@gmail.com>
2017-05-26 16:04:40 -0500

    Added Server and Severs Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,24 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ]
 })
+
 export class AppModule { }
